05a56ceb2d21d6dcccbeb7f72d5243507db6e9ea
Supaluck Singjan <supalucksingjan@gmail.com>
2020-07-23 02:31:31 +0700

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 
     function appendImg(keyword, index) {
         const createImg = document.createElement('img');
+        createImg.src = `https://source.unsplash.com/400x225/?${keyword}&sig=${index}`;
+
+        const galleryDiv = document.querySelector('.gallery');
+        galleryDiv.appendChild(createImg);
     }
 
     function run() {
+        for (let i = 1; i <= 12; i++) {
+            appendImg('dog', i);
+        }
 
     }
 
